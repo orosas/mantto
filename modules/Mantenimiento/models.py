4cb6770baf9dab697cdd946c87dfee6e9c584b6c
orosas <jorosas@gmail.com>
2018-01-08 01:55:12 -0600

    Settings OK, inicio de models

---
+++ modules/Mantenimiento/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+# Create your models here.
+
+class Sitio(models.Model):
+	site = models.CharField(max_length=80, null=False, blank=False)
+	site_name = models.CharField(max_length=100, null=False, blank=False)
+	iden = models.CharField(max_length=80, null=True, blank=True)
+	gsm = models.CharField(max_length=100, null=True, blank=True)
+	id_3g = models.CharField(max_length=100, null=True, blank=True)
+	lte = models.CharField(max_length=100, null=True, blank=True)
+	lat_decimal = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+	long_decimal = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+	site2 = models.CharField(max_length=80, null=True, blank=True)
+	jerarquia = models.CharField(max_length=50, null=True, blank=True)
+	jerarquia_modif = models.CharField(max_length=50, null=True, blank=True)
+	co = models.CharField(max_length=100, null=True, blank=True)
+	co_celular = models.CharField(max_length=100, null=True, blank=True)
+	co_email = models.CharField(max_length=100, null=True, blank=True)
+	coord = models.CharField(max_length=100, null=True, blank=True)
+	coord_celular = models.CharField(max_length=50, null=True, blank=True)
+	coord_email = models.CharField(max_length=100, null=True, blank=True)
+	manager = models.CharField(max_length=100, null=True, blank=True)
+	manager_celular = models.CharField(max_length=50, null=True, blank=True)
+	manager_email = models.CharField(max_length=100, null=True, blank=True)
+	director = models.CharField(max_length=100, null=True, blank=True)
+	director_celular = models.CharField(max_length=50, null=True, blank=True)
+	director_email = models.CharField(max_length=100, null=True, blank=True)
+	coordination = models.CharField(max_length=50, null=True, blank=True)
+	management = models.CharField(max_length=50, null=True, blank=True)
+	region = models.CharField(max_length=20, null=True, blank=True)
+	region_num = models.CharField(max_length=4, null=True, blank=True)
+	mercado_ciudad = models.CharField(max_length=100, null=True, blank=True)
+	mercado_anterior = models.CharField(max_length=100, null=True, blank=True)
+	prioridad = models.CharField(max_length=4, null=True, blank=True)
+	tipo_ante = models.CharField(max_length=50, null=True, blank=True)
+	capacidad_gen = models.CharField(max_length=20, null=True, blank=True)
+	cobertura_min = models.CharField(max_length=100, null=True, blank=True)
+	acceso = models.TextField(null=True, blank=True)
+	address = models.TextField(null=True, blank=True)
+	municipality = models.CharField(max_length=100, null=True, blank=True)
+	state_name = models.CharField(max_length=50, null=True, blank=True)
+
+	def __str__(self):
+		return u"%s" % (self.site_name)
+
